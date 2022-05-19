22ba036c695cdb90df9a1a85e56080181104c3c6
Jason Cameron <jason@alley.co>
2022-05-18 21:37:08 -0400

    services post type updated to show in graphql schema

--- mu-plugins/admin-post-types.php
+++ mu-plugins/admin-post-types.php
@@ -3,8 +3,9 @@
 
 function added_post_types() {
 	register_post_type( 'services', [
-		'public' => true,
-        'show_in_rest' => true,
+		'public'          => true,
+        'show_in_rest'    => true,
+        'show_in_graphql' => true,
 		'labels' => [
 			'name'          => 'Services',
             'add_new_item'  => 'Add New Service',
