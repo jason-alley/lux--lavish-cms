2f6aae110037f542980bb5ece473d27453120d0d
Jason Cameron <jason@alley.co>
2022-05-18 21:25:31 -0400

    services posttype labels added and refactored

--- mu-plugins/admin-post-types.php
+++ mu-plugins/admin-post-types.php
@@ -5,7 +5,10 @@ function added_post_types() {
 	register_post_type( 'services', [
 		'public' => true,
 		'labels' => [
-			'name' => 'Services'
+			'name'         => 'Services',
+            'add_new_item' => 'Add New Service',
+            'edit_item'    => 'Edit Service',
+            'all_item'     => 'All Services'
 		],
 		'menu_icon' => 'dashicons-screenoptions'
 	]);
