4de461d97b3e40cb0cb094f34b881ddc29b51b57
Jason Cameron <jason@alley.co>
2022-05-18 21:41:22 -0400

    services post type updated for graph ql interface

--- mu-plugins/admin-post-types.php
+++ mu-plugins/admin-post-types.php
@@ -6,14 +6,17 @@ function added_post_types() {
 		'public'          => true,
         'show_in_rest'    => true,
         'show_in_graphql' => true,
+        'hierarchical'    => true,
 		'labels' => [
-			'name'          => 'Services',
-            'add_new_item'  => 'Add New Service',
-            'edit_item'     => 'Edit Service',
-            'all_items'     => 'All Services',
-            'singular_name' => 'Event'
+			'name'                => 'Services',
+            'add_new_item'        => 'Add New Service',
+            'edit_item'           => 'Edit Service',
+            'all_items'           => 'All Services',
+            'singular_name'       => 'Event'
 		],
-		'menu_icon' => 'dashicons-screenoptions'
+        'graphql_single_name' => 'document',
+        'graphql_plural_name' => 'documents',
+		'menu_icon'           => 'dashicons-screenoptions'
 	]);
 }
 
