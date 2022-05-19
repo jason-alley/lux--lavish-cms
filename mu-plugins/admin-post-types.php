b72410b5d38eec611c6e436853b0646c1dc0f8fc
Jason Cameron <jason@alley.co>
2022-05-18 22:01:26 -0400

    phpcs update

--- mu-plugins/admin-post-types.php
+++ mu-plugins/admin-post-types.php
@@ -1,25 +1,30 @@
 <?php
 
-
-function added_post_types() {
-	register_post_type( 'services', [
-		'public'          => true,
+/**
+    Registers posts types for admin dashboard.
+ */
+function Admin_Post_types()
+{
+    register_post_type(
+        'services', [
+        'public'          => true,
         'show_in_rest'    => true,
         'show_in_graphql' => true,
         'hierarchical'    => true,
-		'labels' => [
-			'name'                => 'Services',
+        'labels' => [
+        'name'                => 'Services',
             'add_new_item'        => 'Add New Service',
             'edit_item'           => 'Edit Service',
             'all_items'           => 'All Services',
             'singular_name'       => 'Event'
-		],
+        ],
         'graphql_single_name' => 'service',
         'graphql_plural_name' => 'services',
-		'menu_icon'           => 'dashicons-screenoptions'
-	]);
+        'menu_icon'           => 'dashicons-screenoptions'
+        ]
+    );
 }
 
-add_action( 'init', 'added_post_types' );
+add_action('init', 'Admin_Post_types');
 
 ?>
